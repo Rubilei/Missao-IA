c69491a8e904a59dfd85a9d2ba1e2d1130423b41
Rubilei <145511175+Rubilei@users.noreply.github.com>
2024-08-01 11:49:32 -0300

    comit2

---
+++ script.js
@@ -0,0 +1,44 @@
+const caixaPrincipal = documente.querySelector(".caixa-principal");
+const caixaPerguntas = documente.querySelector(".caixa-perguntas");
+const caixaAlternativas = documente.querySelector(".caixa-alternativas");
+const caixaResultado = documente.querySelector(".caixa-resultado");
+const textoResultado = documente.querySelector(".texto-resultado");
+
+const perguntas =     
+[
+    {
+        enunciado: "Pergunta 1", 
+        alternativas:[
+        "Alternativa 1",
+        "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 2", 
+        alternativas:[
+        "Alternativa 1",
+        "Alternativa 2"
+        ] 
+    },
+    {
+        enunciado: "Pergunta 3", 
+        alternativas:[
+        "Alternativa 1",
+        "Alternativa 2"
+        ] 
+    },
+    {
+        enunciado: "Pergunta 4", 
+        alternativas:[
+        "Alternativa 1",
+        "Alternativa 2"
+        ] 
+    },
+    {
+        enunciado: "Pergunta 5", 
+        alternativas:[
+        "Alternativa 1",
+        "Alternativa 2"
+        ] 
+    }
+];
