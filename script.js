fc9d5e0af7e2202280fc216ceb4a93e87f6e4e76
Rubilei <145511175+Rubilei@users.noreply.github.com>
2024-09-05 11:53:15 -0300

    agoravai

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ const perguntas = [
         alternativas: [
             {
                 texto:"Isso é assustador!",
-                Afirmacao:"afirmação"
-            }
+                Afirmacao:"afirmação";
+            },
             {
                 texto:"Isso é maravilhoso!",
                 Afirmacao:"afirmação"
@@ -24,7 +24,7 @@ const perguntas = [
             {
                 texto:  "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
                 afirmacao: "afirmação"
-            }
+            },
             {
                 texto:"Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
                 afirmacao: "afirmação"
@@ -37,9 +37,9 @@ const perguntas = [
             {
             texto:"Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
             afirmacao:"afirmação"
-            }
+            },
             {
-            texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores."
+            texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
             afirmacao: "afirmação"
             }
         ]
@@ -50,8 +50,9 @@ const perguntas = [
             {
             texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
             afirmacao: "afirmação"
-            }
-            texto: "Criar uma imagem utilizando um gerador de imagem de IA."
+            },
+            {
+            texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
             afirmacao: "afirmação"
             }
         ]
@@ -62,9 +63,9 @@ const perguntas = [
             {
             texto:"Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
             afirmacao:"afirmação"
-            }
+            },
             {
-            texto:"O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial."
+            texto:"O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
             afirmacao:"afirmação"
             }
         ]
